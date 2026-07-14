bde2729642f1223f34284462e05068ce250812ca
松本陸玖 <riku.onepiece0106@gmail.com>
2026-07-14 17:52:21 +0900

    受験情報入力フォームを追加

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+
+    score = request.form["score"]
+    faculty = request.form["faculty"]
+    area = request.form["area"]
+
+    return f"""
+    偏差値 : {score}<br>
+    学部 : {faculty}<br>
+    地域 : {area}
+    """
+
 def home():
     return render_template("index.html")
 
